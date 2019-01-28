81f9fea0e249daa2561f02742ab8106bee6546c2
german gomez <germangb42@gmail.com>
2019-01-28 18:18:02 +0100

    add traits crate to prelude

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ use imgui_ext_derive as derive;
 use imgui_ext_traits as traits;
 
 pub mod prelude {
+    pub use imgui_ext_traits;
+    pub use imgui_ext_derive;
     pub use super::derive::ImGuiExt;
     pub use super::traits::{Drag, Input, Simple, Slider, UiExt};
 }
